de6e35362e1425f22f628524fd452baae3d032bf
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-01-01 11:40:11 -0500

    Chapter 1 : Getting Started with Android Programming

---
+++ src/net/learn2develop/HelloWorld/HelloWorldActivity.java
@@ -0,0 +1,14 @@
+package net.learn2develop.HelloWorld;
+
+import net.learn2develop.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
